c64c28913a46ffcbe509fadc7c3f6b0f6f0debae
Princearman777 <sarkerarman25@gmail.com>
2023-11-30 23:59:47 +0600

    Server site complite push

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",
+  "https://assignment-server-plum.vercel.app"],
     credentials: true,
   })
 );
 app.use(cookieParser());
 
 console.log(process.env.USER, process.env.PASS);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ninjyyh.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,9 +38,7 @@ const verifyToken = (req, res, next) => {
   console.log("token in the middleware", req?.cookies?.token);
   // no token available
   if (!token) {
-    return;
-
-    // return res.status(401).send({ message: "unauthorized access" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
     if (err) {
@@ -75,11 +75,35 @@ async function run() {
       const isAdmin = user?.role === "admin";
       console.log(isAdmin, "isadmin ni re");
       if (isAdmin) {
-        console.log("fbhjhsdfgha");
+        next();
+      }
+      // return res.status(403).send({ message: "forbidden access" });
+      // if (isAdmin) {
+      //   console.log("fbhjhsdfgha");
 
+      //   next();
+      //   // return res.status(403).send({ message: "forbidden access" });
+      // }
+    };
+    const verifyVolunteer = async (req, res, next) => {
+      const email = req?.user?.email;
+      console.log("Volunteer ", email);
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      console.log(user);
+      const isVolunteer = user?.role === "Volunteer";
+      console.log(isVolunteer, "isVolunteer ni re");
+      if (isVolunteer) {
+        // return
         next();
         // return res.status(403).send({ message: "forbidden access" });
       }
+      // if (isVolunteer) {
+      //   console.log("fbhjhsdfgha");
+
+      //   next();
+      //   // return res.status(403).send({ message: "forbidden access" });
+      // }
     };
 
     app.post("/jwt", async (req, res) => {
@@ -100,6 +124,7 @@ async function run() {
 
     app.post("/logout", async (req, res) => {
       const user = req.body;
+
       console.log("logging out", user);
       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
@@ -166,6 +191,24 @@ async function run() {
       res.clearCookie("token").send({ success: true });
     });
 
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount);
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
     app.get("/Users", verifyToken, verifyAdmin, async (req, res) => {
       console.log("cheack to token", req?.user?.email);
       // console.log(req.user);
@@ -202,17 +245,61 @@ async function run() {
         res.send(result);
       }
     );
-    app.get("/MyDonationUpdate/:id", async (req, res) => {
-      const id= req.params.id;
-      console.log ("id", id);
+    app.get("/blood_donation_request", async (req, res) => {
+      const donation_status = "pending";
+      const query = {
+        donation_status: donation_status,
+      };
+
+      const result = await DonationRequestCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+    app.post("/SearchValue", async (req, res) => {
+    const search = req.body;
+    console.log("search",search);
+      // const query = {
+      //   donation_status: donation_status,
+      // };
+      console.log(search);
+
+      // const result = await DonationRequestCollection.find(query).toArray();
+      // console.log(result);
+      // res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("id", id);
       const query = {
         _id: new ObjectId(id),
       };
-      console.log(query);
       const result = await DonationRequestCollection.findOne(query);
       console.log(result);
       res.send(result);
     });
+    app.get("/donationDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("id", id);
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await DonationRequestCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+    // app.get("/donationDetails/:id", verifyToken, async (req, res) => {
+ 
+    //   const id = req.params.id;
+    //   console.log("id", id);
+    //   const query = {
+    //     _id: new ObjectId(id),
+    //   };
+    //   console.log(query);
+    //   const result = await DonationRequestCollection.findOne(query);
+    //   console.log(result);
+    //   res.send(result);
+    // });
 
     app.get("/admin/:email", verifyToken, verifyAdmin, async (req, res) => {
       console.log("asoe hlit hocche", req?.user?.email);
@@ -235,28 +322,32 @@ async function run() {
       res.send({ isAdmin });
     });
 
-    app.get("/volunteer/:email", verifyToken, async (req, res) => {
-      console.log("asoe hlit hocche", req?.user?.email);
-      const email = req.params.email;
+    app.get(
+      "/volunteer/:email",
+      verifyToken,
+      verifyVolunteer,
+      async (req, res) => {
+        console.log("asoe hlit hocche", req?.user?.email);
+        const email = req.params.email;
 
-      console.log(req?.user, "emaillllllll", email);
-      if (email !== req?.user?.email) {
-        console.log("provlem");
-        return res.status(403).send({ message: "unauthorized Access" });
+        console.log(req?.user, "emaillllllll", email);
+        if (email !== req?.user?.email) {
+          console.log("provlem");
+          return res.status(403).send({ message: "unauthorized Access" });
+        }
+        const query = { email: email };
+        const user = await UserCollection.findOne(query);
+        console.log("volunteer request user", user?.role);
+        let isVolunteer = false;
+        if (user?.role === "Volunteer") {
+          // isVolunteer = user?.role=='volunteer'
+          isVolunteer = true;
+          console.log(isVolunteer, "volunteer");
+        }
+        console.log("request last ");
+        res.send({ isVolunteer });
       }
-      const query = { email: email };
-      const user = await UserCollection.findOne(query);
-      console.log("volunteer request user", user?.role);
-      let isVolunteer = false;
-      if (user?.role === "Volunteer") {
-        // isVolunteer = user?.role=='volunteer'
-        isVolunteer = true;
-        console.log(isVolunteer, "volunteer");
-      }
-      console.log("request last ");
-      res.send({ isVolunteer });
-    });
-
+    );
     app.get("/request", async (req, res) => {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
@@ -289,39 +380,6 @@ async function run() {
       res.send(result);
     });
 
-    // modarator
-    // app.get("/moderator/:email", verifyToken,verifyVolunteer, async (req, res) => {
-    //   console.log(req?.user?.email);
-    //   const email = req?.params?.email;
-
-    //    console.log(req?.user,"emaillllllll",email);
-    //   if (email !== req?.user?.email) {
-    //     return res.status(403).send({ message: "unauthorized Access" });
-    //   }
-    //   const query = { email: email };
-    //   const user = await UserCollection.findOne(query);
-    //   console.log("Volunteer request user", user?.role);
-    //   let isVolunteer = false;
-    //   if (user.role === "Volunteer") {
-    //     isVolunteer = true;
-    //     console.log(isVolunteer, "the person is moderator");
-    //   }
-    //   res.send({isVolunteer});
-    // });
-    // app.patch("/users/admin/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-
-    //   const UpdatedUser = {
-    //     $set: {
-    //       role: "admin",
-    //     },
-    //   };
-    //   const result = await UserCollection.updateOne(query, UpdatedUser);
-    //   console.log(result);
-
-    //   res.send(result);
-    // });
     app.put("/user-update", verifyToken, verifyAdmin, async (req, res) => {
       const email = req?.query.email;
       const role = req?.body.role;
@@ -339,30 +397,59 @@ async function run() {
       console.log(result);
       res.send(result);
     });
-
-    app.put("/Blood_Request_update/:id",verifyToken, async (req, res) => {
-      const id = req.params.id;
+    app.put("/donar/:id", verifyToken, async (req, res) => {
+      const id = req?.params.id;
+      const donar = req?.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
+      const update = {
+        $set: {
+          donar: donar,
+          donation_status: "inprogress",
+        },
+      };
+      console.log(update,id);
+
+      const result = await DonationRequestCollection.updateOne(
+        filter,
+        update,
+        options
+      );
+      console.log(result);
+      res.send(result);
+    });
+
+    app.put("/Blood_Request_update/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
       const updatedDonation = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      console.log("id", id);
+      console.log("filter", filter);
+
       console.log(updatedDonation);
 
       const Donation = {
         $set: {
-          recipient_name: updatedDonation.recipient_name,
-          address: updatedDonation.address,
-          service_image: updatedDonation.service_image,
-          donation_date: updatedDonation.donation_date,
-          donation_time: updatedDonation.donation_time,
-          hospital_name: updatedDonation.hospital_name,
-          Request_Message: updatedDonation.Request_Message,
-          Upazila: updatedDonation.Upazila,
-          donation_status: updatedDonation.donation_status,
-          requester_Name: updatedDonation.requester_Name,
-          requester_email: updatedDonation.requester_email,
-          requester_photo: updatedDonation.requester_photo,
+          recipient_name: updatedDonation?.recipient_name,
+          address: updatedDonation?.address,
+          District: updatedDonation?.District,
+          Upazila: updatedDonation?.Upazila,
+          hospital_name: updatedDonation?.hospital_name,
+
+          donation_date: updatedDonation?.donation_date,
+          donation_time: updatedDonation?.donation_time,
+          hospital_name: updatedDonation?.hospital_name,
+          Request_Message: updatedDonation?.Request_Message,
+
+          donation_status: updatedDonation?.donation_status,
+          requester_Name: updatedDonation?.requester_Name,
+          requester_email: updatedDonation?.requester_email,
+          requester_photo: updatedDonation?.requester_photo,
         },
       };
+      console.log(Donation);
+
       const result = await DonationRequestCollection.updateOne(
         filter,
         Donation,
@@ -372,12 +459,18 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/donationRequest/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await DonationRequestCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -386,7 +479,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  console.log(res.send("BISTRO BOSS SERVER SITE IS RUNNING"));
+  console.log(res.send("Assignment server is running"));
 });
 app.listen(port, () => {
   console.log(`server is running on this port ${port}`);
